823000e215fe946cde2013294e786ae261071756
KamdemWiyasama <loryannwiyasama@gmail.com>
2026-07-15 01:41:16 -0400

    A Counter with increment, decrement and reset buttons ✅

---
+++ Counter_App/src/Counter.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+function Counter(){
+    const [count, setCount] = useState(0);
+
+    function increment(){
+        setCount(count+1);
+    }
+    function decrement(){
+        setCount(count-1);
+    }
+    function reset(){
+        setCount(0);
+    }
+
+    return(
+        <>
+            <div className='ctn'>
+                <p>{count}</p>
+                <div className="btns">
+                    <button onClick={increment}>Increment</button>
+                    <button onClick={decrement}>Decrement</button>
+                    <button onClick={reset}>Reset</button>
+                </div>
+                
+            </div>
+        </>
+    );
+
+}
+
+export default Counter
